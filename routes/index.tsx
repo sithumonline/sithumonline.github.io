25609203fc20150aced8a7c641861fd1fcc34866
Sithum Bopitiya <sithumsandeepap@gmail.com>
2022-07-03 18:05:05 +0530

    Add icons from cdn

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import * as Icons from "https://cdn.jsdelivr.net/gh/sithumonline/cdn/js/fa/faIcons.js";
 
 export default function Home() {
   const icons = [
@@ -30,15 +31,7 @@ export default function Home() {
     },
     {
       uri: "https://www.facebook.com/sithumonline",
-      Icon: (props: { class?: string }) => (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          class={props.class}
-        >
-          <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z" />
-        </svg>
-      ),
+      Icon: Icons.faFacebookSquare,
     },
     {
       uri: "https://www.instagram.com/sithumonline",
